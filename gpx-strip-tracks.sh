0c87e4fa7880b2f986401be2a7879da9165f3ef8
GpxFeed <info20120901@gmail.com>
2025-07-14 13:07:56 +0200

    Bash Filter Script

--- gpx-strip-tracks.sh
+++ gpx-strip-tracks.sh
@@ -3,6 +3,7 @@
 # A simple example script to delete all track segments in a GPX file set.
 #  - Reads file list from directory "SOURCE"
 #  - calls Python script "gpx-strip-tracks.py"
+#  - tidy makes the remaining code slightly more compact
 #  - writes the stripped files to directory "TARGET"
 
 SOURCE="./gpx"
@@ -11,7 +12,7 @@ TARGET="./gpx-stripped"
 mkdir $TARGET
 
 if ! ( [ -d "$SOURCE" ] && [ -d "$TARGET" ] ) ; then
-  echo missing directory $SOURCE or $TARGET ... exiting.
+  echo missing directory $SOURCE and/or $TARGET ... exiting.
   exit
 fi
 
@@ -19,5 +20,7 @@ for ii in $SOURCE/*.gpx
 do
     GPXFILE=$(basename "$ii")
     echo "$ii >>  $TARGET/$GPX"
-    python3 ./gpx-strip-tracks.py < $ii > $TARGET/$GPXFILE
+    python3 ./gpx-strip-tracks.py < $ii | \
+    tidy -i -xml -utf8 2> /dev/null > $TARGET/$GPXFILE
 done
+
